c1c789e8040d4f4b84420183a4d977c0bb55003c
Max <maxon_p.j@mail.ru>
2022-05-15 01:21:44 +0300

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Max on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("bug fix")
+    }
+
+
+}
+
